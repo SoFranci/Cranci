bbe1adaae0464c4d2f6c2955f080c9f136aed5a5
SoFranci <100066266+SoFranci@users.noreply.github.com>
2022-09-08 21:52:07 +0200

    Update 1.2

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title> cranci.netlify.app</title>
+        <title>cranci.netlify.app</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
